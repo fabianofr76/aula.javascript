b7211ebf48c8c02df3e0e3491fa34a2e26daa82a
fabianofr76 <fabiano.applebees@gmail.com>
2021-10-10 21:44:59 -0300

    aula.java

--- js/main.js
+++ js/main.js
@@ -1,1 +1,74 @@
-alert ("meu primeiro js");
+var d = new Date();
+alert(d.getHours());
+
+/*
+var d = new Date();
+alert(d);
+*/
+/*
+var count;
+for(count=0;count <=5;count++){
+    alert(count);
+};
+*/
+/*
+var count = 0;
+while (count <= 10){
+    console.log(count);
+    alert(count);
+    //count = count +1;
+    count ++;
+};
+*/
+/*
+var idade = prompt("Qual sua idade?");
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+/*
+var idade = 17;
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+/*
+var frutas = [{nome:"maça",cor:"vermelha"},{nome:"uva",cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*
+var fruta = {nome:"maça",cor:"vermelha"}
+//console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+
+//var lista = ["maça","pera","laranja"];
+//lista.pop("uva"); retirar
+//lista.push("uva"); //inserir
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join("-"));
+//console.log(lista[1] );
+//alert(lista[1]);
+//var nome = "Fabiano Freitas";
+//var n1 = 35;
+//var n2 = 10;
+//var frase = "Japao eo melhor time do mundo!!!"
+//alert (nome + "tem"+ idade+"anos");
+//alert (idade+idade2);
+//console.log(nome);
+//console.log(n1*n2);
+//console.log(frase.toLowerCase()); Tudo minusculo
+//console.log(frase.toUpperCase());  Tudo Maiusculo
+//alert(frase.replace("Japao","Brasil")); Substitui japao por Brasil
+
+
+
